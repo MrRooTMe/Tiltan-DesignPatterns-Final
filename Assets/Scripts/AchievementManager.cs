85ddb7577763d2f6a9a1021958ffc05a0f381b6a
avraham1233 <avraham1233@gmail.com>
2021-02-18 21:30:56 +0200

    First Commit
    uploaded project.

---
+++ Assets/Scripts/AchievementManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract class Pickables
+{
+    private string name;
+    private bool isPicked;
+    
+    private List<IListener> listeners = new List<IListener>();
+
+    // Constructor
+    public Pickables(string in_name, bool in_isPicked)
+    {
+        name = in_name;
+        isPicked = in_isPicked;
+
+    }
+
+    public void Attach(IListener listener)
+    {
+        listeners.Add(listener);
+    }
+
+    public void Detach(IListener listener)
+    {
+        listeners.Remove(listener);
+    }
+
+    public void Notify()
+    {
+        foreach (IListener listener in listeners)
+        {
+            listener.UpdateAchievement(this);
+        }
+
+        Debug.Log("Listeners Notified with picked item");
+    }
+
+    public void Achieved()
+    {
+        Notify();
+    }
+
+    public string Name
+    {
+        get { return name; }
+    }
+
+    public bool isPickedObject
+    {
+        get { return isPicked; }
+    }
+
+
+}
+
+/*class WalkedAMile : Pickables
+{
+    public WalkedAMile() : base("walked a mile",
+        "my character had walked a mile",
+        1.5f) { }
+}*/
+
+class Key : Pickables {
+    public Key() : base("SafeKey", true) {  }
+    
+}
+
+
+interface IListener
+{
+    void UpdateAchievement(Pickables isPicked);
+}
+
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GameManager : MonoBehaviour {
+
+
+    private Key key;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        key = new Key();
+   
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            key.Notify();
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            
+        }
+    }
+}
